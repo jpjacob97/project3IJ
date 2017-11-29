d933b942e0d91daaf5d0dba8f7eb2d659302df71
jacobpetersen <jacobpetersen@jacobs-air>
2017-11-28 21:18:15 -0500

    finished all fxml except office

---
+++ fxml/OfficeManagerController.java
@@ -0,0 +1,55 @@
+package projtwo.fxml;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class OfficeManagerController {
+
+    @FXML
+    private Button displayByNameButton;
+
+    @FXML
+    private Button displayByNumButton;
+
+    @FXML
+    private Button checkMinButton;
+
+    @FXML
+    private Button genCommissionButton;
+
+    @FXML
+    private TextField displayByNameField;
+
+    @FXML
+    private TextField displayByNumField;
+
+    @FXML
+    private TextField genComField;
+
+    @FXML
+    private TextArea output;
+
+    @FXML
+    void handleCheckMin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleDisplayByName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleDisplayByNum(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleGenCommission(ActionEvent event) {
+
+    }
+
+}
